57802900bf3ed9452f00354b0795e4332b85a597
Vivaan <69657903+Vivaan-S@users.noreply.github.com>
2022-01-09 00:27:02 +0000

    Fixed starting message, website looking better, login page good.

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
-from flask import Flask, render_template, redirect, request
-import markdown
-app = Flask('app', static_folder="static")
+from flask import Flask, render_template, redirect, request, abort
+import markdown, logging, click
+
+
+
+###################################
+###################################
+###################################
+
+
+
+def secho(text, file = None, nl = None, err = None, color = None, ** styles):
+   pass
+def echo(text, file = None, nl = None, err = None, color = None, ** styles):
+   pass
+
+click.echo = echo
+click.secho = secho
+
+logging.getLogger('werkzeug').setLevel(logging.ERROR)
+app = Flask('Backslash', static_folder="static")
 from json import loads
 feed = []
 
+
+
+###################################
+###################################
+###################################
+
+
+
 redirect_urls = {
   # Format:
-  # "/blabbr" : "https://blabbr.xyz"
+  # "/blabbr" : "https://blabbr.xyz",
   # will redirect /blabbr to blabbr.xyz
 }
 
@@ -24,9 +50,32 @@ rickroll_urls = [
 ] # will rickroll people who follow (hacker and stuff)
 
 
+
+###################################
+###################################
+###################################
+
+
+
+
 @app.route('/')
 def index():
-  return render_template("index.html")
+  return render_template("index.html",
+		user_id=request.headers['X-Replit-User-Id'],
+		user_name=request.headers['X-Replit-User-Name'],
+		user_roles=request.headers['X-Replit-User-Roles']
+  )
+
+@app.route('/login')
+def loginpage():
+  if request.headers['X-Replit-User-Name']:
+    print("  - "+request.headers['X-Replit-User-Name']+" logged in")
+    return redirect("/feed")
+  return render_template("login.html",
+		user_id=request.headers['X-Replit-User-Id'],
+		user_name=request.headers['X-Replit-User-Name'],
+		user_roles=request.headers['X-Replit-User-Roles']
+  )
 
 @app.route('/feed')
 def feedpage():
@@ -36,19 +85,49 @@ def feedpage():
 def newpost():
   data = loads(request.data)
   #print(data)
+  if not request.headers['X-Replit-User-Name']:
+    abort(403)
+
   feed.insert(0,
   {
-    "author": data["author"],
+    "author": request.headers['X-Replit-User-Name'],
     "content": data["content"]
   },
   )
+  print("  - "+data["author"]+": "+data["content"])
   return "success"
+
+
+
+###################################
+
 
 @app.errorhandler(404)
 def four_o_four(e):
   if request.path in rickroll_urls:
-    print("just redirected somebody to a rickroll LOL")
+    print("  - Somebody was rickrolled")
     return redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
   return "", 404
 
+@app.errorhandler(403)
+def four_o_three(e):
+  return "", 403
+
+
+
+###################################
+###################################
+###################################
+
+
+
+print(
+  """
+  * Backslash is now running
+  * Select console and press Ctrl+C/Cmd+C to stop
+  * Messages will be logged below
+  """
+)
+
 app.run(host='0.0.0.0', port=80)
+
